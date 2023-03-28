9d5dc49895fc69ba4b1b6f8055b83bf73fe644d0
YoadTamar <119599940+YoadTamar@users.noreply.github.com>
2023-03-28 17:32:06 +0300

    Update player.cpp

--- sources/player.cpp
+++ sources/player.cpp
@@ -1,1 +1,16 @@
+#include "player.hpp"
+#include "iostream"
+using namespace std;
+
+Player::Player(){
+    
+}
+
+Player::Player(string name)
+{
+    this->name = name;
+}
+int Player::stacksize(){return 0;}
+
+int Player::cardesTaken(){return 0;}
 
