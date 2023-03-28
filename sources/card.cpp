62a116017d356a089c90b8c64484a47f064d533c
YoadTamar <119599940+YoadTamar@users.noreply.github.com>
2023-03-28 16:57:37 +0300

    Update card.cpp

--- sources/card.cpp
+++ sources/card.cpp
@@ -1,1 +1,14 @@
+#pragma once 
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include "card.hpp"
+
+namespace ariel {
+    Card::Card(int value, char type) {
+        this->value = value;
+        this->type = type;
+    }
+}
