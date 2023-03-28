af061a9fca1c222cd5e8f19582a97a1b3235213b
YoadTamar <119599940+YoadTamar@users.noreply.github.com>
2023-03-28 17:13:30 +0300

    Update game.cpp

--- sources/game.cpp
+++ sources/game.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <string>
+#include "game.hpp"
+#include "player.hpp"
+#include "card.hpp"
 
+using namespace std;
+
+namespace ariel
+{
+    Game::Game(Player& other1, Player& other2): p1(p1), p2(p2)
+    {
+        this->_p1 = other1;
+        this->_p1 = other2;
+    }
+
+    void Game::playTurn() {}
+
+    void Game::printLastTurn() {}
+
+    void Game::playAll() {}
+
+    void Game::printWiner() {}
+
+    void Game::printLog() {}
+
+    void Game::printStats() {}
+};
