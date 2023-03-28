68bfdd05a29e0b438dfc2bb9358a47b044c0808f
YoadTamar <119599940+YoadTamar@users.noreply.github.com>
2023-03-28 17:09:00 +0300

    Update game.hpp

--- sources/game.hpp
+++ sources/game.hpp
@@ -1,1 +1,29 @@
+#pragma once
 
+#include "player.hpp"
+
+namespace ariel
+{
+    class Game
+    {
+        private:
+            Player& player1;
+            Player& player2;
+            int turn;
+
+        public:
+            Game(Player& plr1, Player& plr2);
+
+            void playTurn();
+
+            void printLastTurn();
+
+            void playAll();
+
+            void printWiner();
+
+            void printLog();
+
+            void printStats();
+    };
+}
